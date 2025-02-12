9a24c2e3297ed3e662129b826ff28d40c7328676
윤준수 <99115509+hoheesu@users.noreply.github.com>
2025-02-12 21:32:32 +0900

    chore: next.config.ts수정

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false, // Strict Mode 비활성화
+  images: {
+    domains: ['tong.visitkorea.or.kr', 'storage.googleapis.com'], // 허용할 외부 도메인 추가
+  },
 };
 
 export default nextConfig;
