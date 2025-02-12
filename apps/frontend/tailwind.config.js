b15cd9f6a30b343721659379121dffd52df8bd8a
윤준수 <99115509+hoheesu@users.noreply.github.com>
2025-02-12 21:38:21 +0900

    chore: 프론트엔드 배포 수정

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      primary: '#2974E5',
+      white: '#fff',
+      'gray/100': '#E4E4E4',
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
